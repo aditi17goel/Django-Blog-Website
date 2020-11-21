bf898ac3406a527971c0c17eb1e851bb6da9aa25
Aditi Goel <aditi_1901cs04@iitp.ac.in>
2020-11-21 18:20:12 +0530

    updated

--- home/urls.py
+++ home/urls.py
@@ -3,7 +3,12 @@ from django.urls import path, include
 from home import views
 
 urlpatterns = [
-    path('',views.home, name='home'),
-    path('contact/',views.contact, name='contact'),
-    path('about/',views.about, name='about'),
+    path('', views.home, name="home"),
+    path('contact/', views.contact, name="contact"),
+    path('about/', views.about, name="about"),
+    path('search/', views.search, name="search"),
+    path('search/', views.search, name="search"),
+    path('signup', views.handleSignUp, name="handleSignUp"),
+    path('login', views.handeLogin, name="handleLogin"),
+    path('logout/', views.handelLogout, name="handleLogout"),
 ]
--- iCoder/settings.py
+++ iCoder/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,6 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '9f&4v-@c5@k^3$a@=wkuw0!4^(7v@7$9+m@ef^#_mq-d7-5(_6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -36,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home.apps.HomeConfig',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
@@ -117,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MESSAGE_TAGS = {
+    messages.ERROR:'danger'
+}
